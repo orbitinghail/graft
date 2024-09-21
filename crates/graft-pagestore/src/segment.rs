bf8c56081ac49167077579079c4132c06b624fde
Carl Sverre <82591+carlsverre@users.noreply.github.com>
2024-09-20 18:27:49 -0700

    dreaming about segment storage

--- crates/graft-pagestore/src/segment.rs
+++ crates/graft-pagestore/src/segment.rs
@@ -1,5 +1,6 @@
 pub mod bus;
 pub mod closed;
+pub mod manager;
 pub mod open;
 pub mod uploader;
 pub mod writer;
---
+++ crates/graft-pagestore/src/segment/manager.rs
@@ -0,0 +1,4 @@
+//! The Segment Manager Task is responsible for managing the Segment cache and
+//! making Segment metadata available to other Tasks.
+
+pub struct SegmentManagerTask {}
