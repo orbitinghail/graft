4ffb81a82b2026489ef0982d2f27080abb48a9af
Carl Sverre <82591+carlsverre@users.noreply.github.com>
2025-07-08 16:19:49 -0700

    cleaner codec and fjall key impls

--- crates/graft-core/src/snapshot.rs
+++ crates/graft-core/src/snapshot.rs
@@ -1,5 +1,21 @@
-use crate::codec;
+use bilrost::Message;
 
-pub type Snapshot = codec::v1::remote::Snapshot;
+use crate::{PageCount, VolumeId, lsn::LSN};
+
+/// A Volume Snapshot.
+#[derive(Debug, Clone, Message, PartialEq, Eq)]
+pub struct Snapshot {
+    /// The Volume's ID
+    #[bilrost(1)]
+    vid: VolumeId,
+
+    /// The Snapshot LSN
+    #[bilrost(2)]
+    lsn: LSN,
+
+    /// The Volume's `PageCount` at this LSN.
+    #[bilrost(3)]
+    page_count: PageCount,
+}
 
 impl Snapshot {}
--- crates/graft-core/src/volume_control.rs
+++ crates/graft-core/src/volume_control.rs
@@ -1,5 +1,37 @@
-use crate::codec;
+use std::time::SystemTime;
 
-pub type VolumeControl = codec::v1::remote::VolumeControl;
+use bilrost::Message;
 
-impl VolumeControl {}
+use crate::{VolumeId, volume_ref::VolumeRef};
+
+/// A Volume has a top level control file stored at
+/// `{prefix}/{vid}/control`
+/// Control files are immutable.
+#[derive(Debug, Clone, Message, PartialEq, Eq)]
+pub struct VolumeControl {
+    /// The Volume's ID
+    #[bilrost(1)]
+    vid: VolumeId,
+
+    /// The parent reference if this Volume is a fork.
+    #[bilrost(2)]
+    parent: Option<VolumeRef>,
+
+    /// The creation timestamp of this Volume.
+    #[bilrost(3)]
+    created_at: SystemTime,
+}
+
+impl VolumeControl {
+    pub fn new(vid: VolumeId, parent: Option<VolumeRef>, created_at: SystemTime) -> Self {
+        Self { vid, parent, created_at }
+    }
+
+    pub fn vid(&self) -> &VolumeId {
+        &self.vid
+    }
+
+    pub fn parent(&self) -> Option<&VolumeRef> {
+        self.parent.as_ref()
+    }
+}
