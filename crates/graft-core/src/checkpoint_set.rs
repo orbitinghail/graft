4ae1947adde245784dd14b46d1d5ba3798ed895c
Carl Sverre <82591+carlsverre@users.noreply.github.com>
2025-07-08 16:19:49 -0700

    cleaner codec and fjall key impls

--- crates/graft-core/src/checkpoint_set.rs
+++ crates/graft-core/src/checkpoint_set.rs
@@ -1,6 +1,20 @@
-use crate::{VolumeId, codec, lsn::LSN};
+use bilrost::Message;
+use smallvec::SmallVec;
 
-pub type CheckpointSet = codec::v1::remote::CheckpointSet;
+use crate::{VolumeId, lsn::LSN};
+
+/// A Volume's `CheckpointSet` is stored at `{prefix}/{vid}/checkpoints`.
+/// `CheckpointSets` are updated by the checkpointer via compare-and-swap.
+#[derive(Debug, Clone, Message, PartialEq, Eq)]
+pub struct CheckpointSet {
+    /// The ID of the Volume containing this `CheckpointSet`
+    #[bilrost(1)]
+    vid: VolumeId,
+
+    /// The set of checkpoint LSNs.
+    #[bilrost(2)]
+    lsns: SmallVec<[LSN; 2]>,
+}
 
 impl CheckpointSet {
     pub fn new(vid: VolumeId, lsns: &[LSN]) -> Self {
--- crates/graft-core/src/volume_handle.rs
+++ crates/graft-core/src/volume_handle.rs
@@ -1,5 +1,38 @@
-use crate::codec;
+use bilrost::Message;
 
-pub type VolumeHandle = codec::v1::local::VolumeHandle;
+use crate::{commit_hash::CommitHash, handle_id::HandleId, lsn::LSN, volume_ref::VolumeRef};
 
-impl VolumeHandle {}
+#[derive(Debug, Clone, Message, PartialEq, Eq)]
+pub struct VolumeHandle {
+    /// The Handle ID
+    #[bilrost(1)]
+    id: HandleId,
+
+    /// Reference to the latest synchronization point for the local Volume.
+    #[bilrost(2)]
+    local: VolumeRef,
+
+    /// Reference to the latest synchronization point for the remote Volume.
+    #[bilrost(3)]
+    remote: Option<VolumeRef>,
+
+    /// Presence of the `pending_commit` field means that the Push operation is in
+    /// the process of committing to the remote. If no such Push job is currently
+    /// running (i.e. it was interrupted), this field must be used to resume or
+    /// abort the commit process.
+    #[bilrost(4)]
+    pending_commit: Option<PendingCommit>,
+}
+
+#[derive(Debug, Clone, Message, PartialEq, Eq)]
+pub struct PendingCommit {
+    /// The resulting remote LSN that the push job is attempting to create.
+    #[bilrost(1)]
+    remote_lsn: LSN,
+
+    /// The associated commit hash. This is used to determine whether or not the
+    /// commit has landed in the remote, in the case that we are interrupted
+    /// while attempting to push.
+    #[bilrost(2)]
+    commit_hash: CommitHash,
+}
