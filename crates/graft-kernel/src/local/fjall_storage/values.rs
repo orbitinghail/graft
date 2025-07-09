cd0cb55220396dc090da04278cb755981c1b6193
Carl Sverre <82591+carlsverre@users.noreply.github.com>
2025-07-09 10:51:12 -0700

    tests for fjall repr values

--- crates/graft-kernel/src/local/fjall_storage/values.rs
+++ crates/graft-kernel/src/local/fjall_storage/values.rs
@@ -50,3 +50,60 @@ macro_rules! impl_fjallrepr_for_bilrost {
 }
 
 impl_fjallrepr_for_bilrost!(VolumeHandle, VolumeMeta, Commit);
+
+#[cfg(test)]
+mod tests {
+    use graft_core::{
+        PageCount, VolumeId, handle_id::HandleId, lsn::LSN, page::PAGESIZE, snapshot::Snapshot,
+        volume_ref::VolumeRef,
+    };
+    use smallvec::{SmallVec, smallvec};
+
+    use crate::local::fjall_storage::fjall_repr::testutil::{
+        test_empty_default, test_invalid, test_roundtrip,
+    };
+
+    use super::*;
+
+    #[graft_test::test]
+    fn test_page() {
+        test_roundtrip(Page::test_filled(123));
+        test_invalid::<Page>(b"");
+        test_invalid::<Page>(&b"a".repeat(PAGESIZE.as_usize() + 1));
+    }
+
+    #[graft_test::test]
+    fn test_volume_handle() {
+        test_roundtrip(VolumeHandle::new(
+            HandleId::new("test-handle").unwrap(),
+            VolumeRef::new(VolumeId::random(), LSN::new(123)),
+            None,
+            None,
+        ));
+        test_empty_default::<VolumeHandle>();
+        test_invalid::<VolumeHandle>(&b"abc".repeat(123));
+    }
+
+    #[graft_test::test]
+    fn test_volume_meta() {
+        test_roundtrip(VolumeMeta::new(
+            VolumeId::random(),
+            Some(VolumeRef::new(VolumeId::random(), LSN::new(123))),
+            Some(Bytes::from_static(b"asdf")),
+            smallvec![LSN::new(123)],
+        ));
+        test_empty_default::<VolumeMeta>();
+        test_invalid::<VolumeMeta>(&b"abc".repeat(123));
+    }
+
+    #[graft_test::test]
+    fn test_commit() {
+        test_roundtrip(Commit::new(
+            VolumeId::random(),
+            LSN::new(123),
+            PageCount::new(456),
+        ));
+        test_empty_default::<Commit>();
+        test_invalid::<Commit>(&b"abc".repeat(123));
+    }
+}
