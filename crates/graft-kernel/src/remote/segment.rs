8c0b53b3c14af2a262a57343e5357e6cb98b25b7
Carl Sverre <82591+carlsverre@users.noreply.github.com>
2025-10-14 08:59:05 -0700

    add zstd example

--- crates/graft-kernel/src/remote/segment.rs
+++ crates/graft-kernel/src/remote/segment.rs
@@ -1,14 +1,35 @@
 /*
- * Segments are sequences of compressed frames. All pages in a Segment is stored in order by `PageIdx`.
- * Consider using zstd's trailing checksums.
- * Building a Segment needs to also emit a SegmentIdx
- *
- * https://github.com/carlsverre/rust-compression-playground
- *
- * Ok so my plan is to go with compressing frames at a time using zstd directly.
- * I need to make sure this integrates well into the object storage upload api.
- * I think an iterator of frames should work well for that.
- */
+* Segments are sequences of compressed frames. All pages in a Segment is stored in order by `PageIdx`.
+* Consider using zstd's trailing checksums.
+* Building a Segment needs to also emit a SegmentIdx
+*
+* https://github.com/carlsverre/rust-compression-playground
+*
+* Ok so my plan is to go with compressing frames at a time using zstd directly.
+* I need to make sure this integrates well into the object storage upload api.
+* I think an iterator of frames should work well for that.
+*
+*
+fn run_zstd(frame_data: &[u8]) {
+    let mut compressed = File::create("data.zstd").unwrap();
+    let mut compressor = Compressor::new(3).unwrap();
+
+    compressor.include_checksum(true).unwrap();
+    compressor.include_contentsize(true).unwrap();
+
+    let mut buf = Vec::with_capacity(compress_bound(frame_data.len()));
+
+    for _ in 0..FRAMES {
+        buf.clear();
+        compressor
+            .compress_to_buffer(&frame_data, &mut buf)
+            .unwrap();
+        compressed.write_all(&buf).unwrap();
+    }
+
+    compressed.flush().unwrap();
+}
+*/
 
 /// The maximum number of pages per Frame
 const FRAME_MAX_PAGES: usize = 64;
